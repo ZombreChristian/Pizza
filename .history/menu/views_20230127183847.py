5ff4eef679246a190be29c9dc5bf709b93ad34f2
ZombreChristian <christianzombre346@gmail.com>
2023-02-02 13:28:32 +0100

    ajout du projet pizza

---
+++ .history/menu/views_20230127183847.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+
+# Create your views here.
+
+# EXERCICE
+# Les pizzas : Végétarienne : 8.5€, Poulet champignons : xxx, 4 fromages, Carnivore
+
+
+def index(request):
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
